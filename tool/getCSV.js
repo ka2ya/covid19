ca39157bc2fafc74c17dafd1e15f3657a3da6336
Yusei Ito <yuseikun@Outlook.jp>
2020-04-10 21:59:13 +0900

    Create: CSV download

---
+++ tool/getCSV.js
@@ -0,0 +1,33 @@
+const request = require('request')
+
+const getCSV = function(csvType) {
+  require('dotenv').config()
+  const url = process.env[csvType + '_CSV_URL']
+  const options = {
+    url,
+    method: 'GET',
+    followAllRedirects: true // 3xxが返ってきたとき、リダイレクトする。
+  }
+
+  return new Promise(function(resolve) {
+    request(options, function(_error, _response, body) {
+      let obj = body
+      obj = body.split('\r\n')
+      const columns = obj[0].split(',')
+
+      for (let i = 1; i < obj.length; i++) {
+        const tmp = {}
+        columns.forEach((e, index) => {
+          tmp[e] = obj[i].split(',')[index]
+        })
+
+        obj[i] = tmp
+      }
+      obj.shift()
+
+      resolve(obj)
+    })
+  })
+}
+
+module.exports = getCSV
