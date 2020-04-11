f9d39db2424ee6264d1163d60841c6f7ffcc6c5c
Yusei Ito <yuseikun@Outlook.jp>
2020-04-11 15:10:18 +0900

    Create consults numebr card
    ConfirmedCasesNumberCardがベースです。帰国者接触者専門外来の受診者数を表示します。

--- components/cards/ConsultsNumberCard.vue
+++ components/cards/ConsultsNumberCard.vue
@@ -1,12 +1,12 @@
 <template>
   <v-col cols="12" md="6" class="DataCard">
     <time-bar-chart
-      :title="$t('陽性患者数')"
-      :title-id="'number-of-confirmed-cases'"
-      :chart-id="'time-bar-chart-patients'"
-      :chart-data="patientsGraph"
-      :date="Data.patients.date"
-      :unit="$t('人')"
+      :title="$t('帰国者・接触者専門外来受診件数')"
+      :title-id="'number-of-consultation'"
+      :chart-id="'time-bar-chart-consulation'"
+      :chart-data="consultsGraph"
+      :date="Data.consults.date"
+      :unit="$t('件')"
     />
   </v-col>
 </template>
@@ -14,28 +14,8 @@
 <i18n>
 {
   "ja": {
-    "陽性患者数": "陽性患者数",
-    "人": "人"
-  },
-  "en": {
-    "陽性患者数": "Number of confirmed cases",
-    "人": "persons"
-  },
-  "zh-cn": {
-    "陽性患者数": "确诊人数",
-    "人": "人"
-  },
-  "zh-tw": {
-    "陽性患者数": "確診人數",
-    "人": "人"
-  },
-  "ko": {
-    "陽性患者数": "확진자 수",
-    "人": "인"
-  },
-  "ja-basic": {
-    "陽性患者数": "びょうきの ひとの かず",
-    "人": "にん"
+    "帰国者・接触者専門外来受診件数": "帰国者・接触者専門外来受診件数",
+    "件": "件"
   }
 }
 </i18n>
@@ -50,12 +30,13 @@ export default {
     TimeBarChart
   },
   data() {
-    // 感染者数グラフ
-    const patientsGraph = formatGraph(Data.patients_summary.data)
+    // 受診数グラフ
+    console.log(Data)
+    const consultsGraph = formatGraph(Data.consults.data)
 
     const data = {
       Data,
-      patientsGraph
+      consultsGraph
     }
     return data
   }
