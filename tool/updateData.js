b4580d10e4b2e4ca77074b2b097b8c2c4ba14c4b
Yusei Ito <yuseikun@Outlook.jp>
2020-04-13 14:12:51 +0900

    プロパティ名を誤ったので修正

--- tool/updateData.js
+++ tool/updateData.js
@@ -194,7 +194,7 @@ Promise.all([
   testsPromise,
   querentsPromise
 ]).then(() => {
-  data.latestUpdate = moment().format('YYYY\\/MM\\/DD HH:mm')
+  data.lastUpdate = moment().format('YYYY\\/MM\\/DD HH:mm')
   fs.writeFileSync('./data/data.json', JSON.stringify(data, null, 2))
 })
 
