9f8f2eb4e8e96370873a0ff7e560c78538d5a3be
Yusei Ito <yuseikun@Outlook.jp>
2020-04-19 16:09:04 +0900

    一般相談件数カードを追加
    帰国者・接触者相談センターのものがベース

---
+++ components/cards/GeneralQuerentsNumberCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-col cols="12" md="6" class="DataCard">
+    <time-bar-chart
+      :title="$t('一般相談 相談件数')"
+      :title-id="'number-of-general-querents'"
+      :chart-id="'time-bar-chart-number-of-general-querents'"
+      :chart-data="generalQuerentsGraph"
+      :date="Data.generalQuerents.date"
+      :unit="$t('件')"
+    />
+  </v-col>
+</template>
+
+<i18n>
+{
+  "ja": {
+    "一般相談 相談件数": "一般相談 相談件数",
+    "件": "件"
+  }
+}
+</i18n>
+
+<script>
+import Data from '@/data/data.json'
+import formatGraph from '@/utils/formatGraph'
+import TimeBarChart from '@/components/TimeBarChart.vue'
+
+export default {
+  components: {
+    TimeBarChart
+  },
+  data() {
+    // 一般相談 相談数グラフ
+    const generalQuerentsGraph = formatGraph(Data.generalQuerents.data)
+
+    const data = {
+      Data,
+      generalQuerentsGraph
+    }
+    return data
+  }
+}
+</script>
