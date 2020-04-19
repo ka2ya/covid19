f61b039d63a9ba006f5dcde795284164b597c4ae
Yusei Ito <yuseikun@Outlook.jp>
2020-04-19 16:10:16 +0900

    ページに一般相談件数を導入

--- pages/index.vue
+++ pages/index.vue
@@ -12,6 +12,7 @@
       <consults-number-card />
       <tests-number-card />
       <querents-number-card />
+      <general-querents-number-card />
       <line-invitation-card />
     </v-row>
   </div>
@@ -31,6 +32,7 @@ import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttri
 import ConsultsNumberCard from '@/components/cards/ConsultsNumberCard'
 import TestsNumberCard from '@/components/cards/TestsNumberCard'
 import QuerentsNumberCard from '@/components/cards/QuerentsNumberCard'
+import GeneralQuerentsNumberCard from '@/components/cards/GeneralQuerentsNumberCard'
 import LineInvitationCard from '@/components/cards/LineInvitationCard'
 
 export default {
@@ -42,6 +44,7 @@ export default {
     ConsultsNumberCard,
     TestsNumberCard,
     QuerentsNumberCard,
+    GeneralQuerentsNumberCard,
     LineInvitationCard
   },
   data() {
